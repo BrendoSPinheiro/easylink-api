3c013082e8aa00d4a27ee100658181179af968d7
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-29 10:46:17 -0300

    chore: :sparkles: configure routes archive and use in server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
+app.use(routes);
 
 app.listen(3001, () => {
   console.log('🔥 server started at http://localhost:3001');
