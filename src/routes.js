76e18c9d5f900fd9a7b24dd2317ea649f566b5f1
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-30 11:54:36 -0300

    feat: :sparkles: add auth middleware and delete user dto

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const { Router } = require('express');
 const SessionController = require('./app/controllers/SessionController');
 const UserController = require('./app/controllers/UserController');
 
+const authMiddleware = require('./app/middlewares/authMiddleware');
+
 const router = Router();
 
 router.post('/users', UserController.store);
-router.put('/users/:id', UserController.update);
+router.put('/users/:id', authMiddleware, UserController.update);
 
 router.post('/sessions', SessionController.authenticate);
 
