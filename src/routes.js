b69ddbc6c83e188ab22f903836c5299931dbfe4e
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-02-04 13:28:42 -0300

    feat: :sparkles: register route to update category

--- src/routes.js
+++ src/routes.js
@@ -17,5 +17,6 @@ router.use(authMiddleware);
 router.get('/categories', CategoryController.index);
 router.get('/categories/:id', CategoryController.show);
 router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
