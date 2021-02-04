2082d1ac897b47c67a456ff63637afd4310ec9fa
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-02-04 13:48:48 -0300

    feat: :sparkles: add update controller in contact controller

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -50,6 +50,36 @@ class CategoryController {
 
     response.json(category);
   }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+    const { userId } = request;
+
+    const categoryExists = await CategoryRepository.findById(id);
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+    });
+
+    if (!await schema.isValid({ name })) {
+      return response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const categoryByName = await CategoryRepository.findByNameAndUserId(
+      name, userId,
+    );
+    if (categoryByName) {
+      return response.status(400).json({ error: 'This name already in use' });
+    }
+
+    const category = await CategoryRepository.update(id, { name });
+
+    response.json(category);
+  }
 }
 
 module.exports = new CategoryController();
