5fbe6f7974544a3bd219cc557eba625a68bda98e
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-02-04 14:29:46 -0300

    feat: :sparkles: add validation to check if the user owns the category

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -60,6 +60,11 @@ class CategoryController {
     if (!categoryExists) {
       return response.status(404).json({ error: 'Category not found' });
     }
+    if (categoryExists && categoryExists.user_id !== userId) {
+      return response.status(400).json({
+        error: 'This category belongs to another user',
+      });
+    }
 
     const schema = yup.object().shape({
       name: yup.string().required(),
